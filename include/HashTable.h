edda72444c8013e4a11a7f6f96c363a6b2105f08
Wiem AYARI <wiem.ayari@etudiant-isi.utm.tn>
2026-01-29 16:49:42 +0100

    Add Scheme struct and HashTable class definition
    Define Scheme struct and HashTable class with basic methods.

--- include/HashTable.h
+++ include/HashTable.h
@@ -1,1 +1,16 @@
+// include/HashTable.hpp
+#include <string>
 
+struct Scheme {
+    std::string nom;      // ex: "مفعول" [cite: 71]
+    std::string regle;    // Représentation abstraite [cite: 73]
+};
+
+class HashTable {
+private:
+    Scheme* table[100]; // Implémentation manuelle demandée [cite: 32]
+public:
+    int hashFunction(std::string key);
+    void insert(std::string nom, std::string regle); // [cite: 34]
+    std::string getRegle(std::string nom);           // Accès direct [cite: 33]
+};
