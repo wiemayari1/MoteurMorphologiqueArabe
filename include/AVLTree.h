ecca8cbfa0f836a5b042ffecc76b10857d322b2e
Ayari Wiem <wiem.ayari@etudiant-isi.utm.tn>
2026-01-30 18:52:04 +0100

    Remove AVLTree header file and its contents

--- include/AVLTree.h
+++ include/AVLTree.h
@@ -1,18 +1,1 @@
-// include/AVLTree.hpp
-#include <string>
-#include <vector>
 
-struct Node {
-    std::string racine;             // ex: "كتب" [cite: 64]
-    std::vector<std::string> derives; // Liste des mots validés [cite: 66]
-    int frequence;                  // [cite: 68]
-    Node *left, *right;
-    int height;
-};
-
-class AVLTree {
-public:
-    void insert(std::string r);    // Insertion dynamique [cite: 28]
-    Node* search(std::string r);   // Recherche logarithmique 
-    void display();                 // Affichage structuré [cite: 30]
-};
