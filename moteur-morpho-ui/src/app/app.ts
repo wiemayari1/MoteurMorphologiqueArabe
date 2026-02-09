cce1fcd0e80abddc8f6763d1a540c1fd83b6ad0a
Ayari Wiem <wiem.ayari@etudiant-isi.utm.tn>
2026-02-09 19:47:18 +0100

    Add MatButtonModule and fix styleUrls syntax

--- moteur-morpho-ui/src/app/app.ts
+++ moteur-morpho-ui/src/app/app.ts
@@ -5,6 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +14,14 @@ import { MatIconModule } from '@angular/material/icon';
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
+
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
     MatIconModule,
+    MatButtonModule,
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
