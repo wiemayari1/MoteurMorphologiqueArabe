f1cd0751043e6be5705f074c8888a1ee3c46b4be
Ayari Wiem <wiem.ayari@etudiant-isi.utm.tn>
2026-02-10 08:23:43 +0100

    Add routing configuration for application pages

---
+++ moteur-morpho-ui/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home/home-page.component';
+import { RootsPageComponent } from './pages/roots/roots-page.component';
+import { SchemesPageComponent } from './pages/schemes/schemes-page.component';
+import { GeneratePageComponent } from './pages/generate/generate-page.component';
+import { ValidatePageComponent } from './pages/validate/validate-page.component';
+import { GamePageComponent } from './pages/game/game-page.component';
+
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'roots', component: RootsPageComponent },
+  { path: 'schemes', component: SchemesPageComponent },
+  { path: 'generate', component: GeneratePageComponent },
+  { path: 'validate', component: ValidatePageComponent },
+  { path: 'game', component: GamePageComponent },
+  { path: '**', redirectTo: '' }
+];
