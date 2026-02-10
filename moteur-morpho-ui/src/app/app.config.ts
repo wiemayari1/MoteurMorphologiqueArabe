a4f2cb9c67caf8fe83a9ee608e06074222dd170a
Ayari Wiem <wiem.ayari@etudiant-isi.utm.tn>
2026-02-10 08:22:29 +0100

    Add application configuration for Angular app

---
+++ moteur-morpho-ui/src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { Directionality } from '@angular/cdk/bidi';
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimations(),
+    { provide: Directionality, useValue: { value: 'rtl', change: null } }
+  ]
+};
